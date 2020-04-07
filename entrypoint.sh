d63890c8b1424c9cd39fff9033d3d67a5c1c4d84
Matthias Pigulla <mp@webfactory.de>
2020-04-07 16:06:32 +0000

    Add env vars to change dir permissions/ownership on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@
 
 sh -c 'while true; do sleep $PURGE_INTERVAL; bin/console hotfolder:purge "$ARCHIVE" ; done' &
 
+[ -z "$HOTFOLDER_OWNER" ] || chown "$HOTFOLDER_OWNER" $HOTFOLDER
+[ -z "$HOTFOLDER_PERM" ] || chmod "$HOTFOLDER_PERM" $HOTFOLDER
+
 while true; do
     inotifywait --quiet --timeout 10 --event create,modify --recursive $HOTFOLDER || true 
     bin/console hotfolder:upload --form-field-name=$FORM_FIELD_NAME $UPLOAD_URL $HOTFOLDER "$PATTERN" $ARCHIVE
